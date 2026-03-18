e8feb3a1953a52e246cd06c67954b35e4147554c
mhs0018 <mhs0018@alu.medac.es>
2026-03-18 12:38:26 +0100

    Actividad 14 final

--- src/test/java/instituto/ValidadorRegistroTest.java
+++ src/test/java/instituto/ValidadorRegistroTest.java
@@ -5,19 +5,56 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ValidadorRegistroTest {
-    // Traemos el "motor" que vamos a probar desde la carpeta main
     ValidadorRegistro validador = new ValidadorRegistro();
 
     @Test
-    @DisplayName("Reto 1: Nombre vacío")
-    public void testNombreValido() {
-            // Ejecutamos la lógica con un nombre correcto
-            boolean resultado = validador.validarNombre("Carlos");
+    @DisplayName("Reto 1: Nombre vacio")
+    public void testNombreVacio() {
+        String nombreVacio = "";
 
-            // Sentenciamos: Como el nombre está bien, esperamos que el resultado sea TRUE
-            assertTrue(resultado, "Error: El sistema rechazó un nombre válido");
-        }
+        boolean resultado = validador.validarNombre(nombreVacio);
 
-        // ESCRIBE TUS 5 RETOS A PARTIR DE AQUÍ
-
+        assertFalse(resultado, "Error: El sistema rechazó el nombre");
     }
+
+    @Test
+    @DisplayName("Reto 2: Password justa")
+    public void testPaswordJusta() {
+        String password = "12345678";
+
+        boolean resultado = validador.validarPassword(password);
+
+        assertTrue(resultado, "La contraseña es aceptada");
+    }
+
+    @Test
+    @DisplayName("Reto 3: Password corta")
+    public void testPasswordCorta() {
+        String password = "Amdmin";
+
+        boolean resultado = validador.validarPassword(password);
+
+        assertFalse(resultado, "Error: El sistema rechazó la contraseña");
+    }
+
+    @Test
+    @DisplayName("Reto 4: Email sin arroba")
+    public void testEmailSinArroba() {
+        String email = "usuario.gmail.com";
+
+        boolean resultado = validador.validarEmail(email);
+
+        assertFalse(resultado, "Error: El sistema rechazó el email");
+    }
+
+    @Test
+    @DisplayName("Reto 5: Edad Limite")
+    public void testEdadLimite() {
+        int edad = 16;
+        boolean resultado = validador.validarEdad(edad);
+
+        // Sentenciamos: Como el nombre está bien, esperamos que el resultado sea TRUE
+        assertTrue(resultado, "La edad es correcta");
+    }
+
+}
